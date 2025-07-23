bfe303f7fc066cb486b3e77453617053fd3d103f
Avanish Khachane <avanishkhachane@gmail.com>
2025-07-23 15:31:21 +0530

    removed redundant folder

--- Trees/gitpushFromTree.ps1
+++ Trees/gitpushFromTree.ps1
@@ -7,6 +7,6 @@ if ($Message -eq "") {
 }
 
 cd..
-cd..
+
 .\gitpush.ps1 "$Message"
 # This script changes the directory to the parent directory twice and then calls another PowerShell script to push changes to a Git repository.
