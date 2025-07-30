140fd68b58072ae3ff3aff38fc8aae3e9b10a149
AvanishKhachane <avanishkhachane19@gmail.com>
2025-07-30 15:31:29 +0530

    updated git push files

--- Trees/gitpushFromTree.ps1
+++ Trees/gitpushFromTree.ps1
@@ -3,14 +3,13 @@ param
     [string]$Message = "Auto commit"
 )
 
-if ($Message -eq "") 
-{
+if ($Message -eq "") {
     $Message = "Auto commit"
 }
 
-cd..
+Set-Location ..
 
 .\gitpush.ps1 "$Message"
 # This script changes the directory to the parent directory twice and then calls another PowerShell script to push changes to a Git repository.
 
-cd Trees
+Set-Location (Split-Path -Leaf $PSScriptRoot)
