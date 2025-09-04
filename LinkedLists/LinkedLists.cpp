073c7ccbfaf931fa0028a070c6f10ef66db86dd6
AvanishKhachane <avanishkhachane19@gmail.com>
2025-09-04 22:53:01 +0530

    Auto Commit

--- LinkedLists/LinkedLists.cpp
+++ LinkedLists/LinkedLists.cpp
@@ -1,81 +1,102 @@
 #include <memory>
 #include <stdexcept>
 
-template<typename T = int>
-struct node {
+template <typename T = int>
+struct node
+{
 private:
 	T data;
 	std::unique_ptr<node<T>> next;
-public:
 
-	node(T val, node<T>* nextNode = nullptr) : data(val) {
-		if (nextNode) { next = std::unique_ptr<struct node<T>>(nextNode); }
+public:
+	node(T val, node<T> *nextNode = nullptr) : data(val)
+	{
+		if (nextNode)
+		{
+			next = std::unique_ptr<struct node<T>>(nextNode);
+		}
 	}
 
-	node(const node<T>& other) : data(other.data) {
-		if (other.next) {
+	node(const node<T> &other) : data(other.data)
+	{
+		if (other.next)
+		{
 			next = std::unique_ptr<struct node<T>>(*other.next);
 		}
-		else {
+		else
+		{
 			next = nullptr;
 		}
 	}
 
 	T getvalue() const { return data; };
 
-	T setvalue(T val) {
+	T setvalue(T val)
+	{
 		data = val;
 		return data;
 	}
 
-	void setnextnode(node<T>& nextNode)
-	{	
-		next = std::unique_ptr<struct node<T>>(*nextNode);
+	void setnextnode(node<T> &nextNode)
+	{
+		next = std::unique_ptr<struct node<T>>(nextNode);
 	}
 
-	node<T>* getnextnode() { return next.get(); }
+	node<T> *getnextnode() { return next.get(); }
 };
 
-template<typename T = int>
-class linkedlist {
+template <typename T = int>
+class linkedlist
+{
 private:
 	std::unique_ptr<node<T>> head;
-	node<T>* tail; // raw pointer, not unique_ptr
+	node<T> *tail; // raw pointer, not unique_ptr
 	int size;
+
 public:
 	linkedlist() : head(nullptr), tail(nullptr), size(0) {}
 
-	void add(T val) {
+	void add(T val)
+	{
 		std::unique_ptr<node<T>> newNode = std::make_unique<node<T>>(val);
 
-		if (!head) {
+		if (!head)
+		{
 			head = std::move(newNode);
 			tail = head.get();
 		}
-		else {
+		else
+		{
 			tail->setnextnode(*(newNode.get()));
 			tail = tail->getnextnode();
 		}
 		size++;
 	}
 
-	void remove(T val) {
-		if (!head) return;
-		node<T>* current = head.get();
-		while (current->getnextnode() && current->getnextnode()->getvalue() != val) {
+	void remove(T val)
+	{
+		if (!head)
+			return;
+		node<T> *current = head.get();
+		while (current->getnextnode() && current->getnextnode()->getvalue() != val)
+		{
 			current = current->getnextnode();
 		}
-		if (current->getnextnode() && current->getnextnode()->getvalue() == val) {
-			node<T>* toDelete = current->getnextnode();
+		if (current->getnextnode() && current->getnextnode()->getvalue() == val)
+		{
+			node<T> *toDelete = current->getnextnode();
 			current->setnextnode(*(toDelete->getnextnode()));
-			if (toDelete == tail) {
+			if (toDelete == tail)
+			{
 				tail = current;
 			}
 			size--;
 		}
-		else if (head->getvalue() == val) { // Special case for head
+		else if (head->getvalue() == val)
+		{ // Special case for head
 			head = std::unique_ptr<struct node<T>>(head->getnextnode());
-			if (!head) {
+			if (!head)
+			{
 				tail = nullptr;
 			}
 			size--;
@@ -83,21 +104,25 @@ public:
 	}
 
 	int getsize() const { return size; }
-	node<T>* gethead() const { return head.get(); }
-	node<T>* gettail() const { return tail; }
+	node<T> *gethead() const { return head.get(); }
+	node<T> *gettail() const { return tail; }
 
-	node<T>* getnode(int index) const {
-		if (index < 0 || index >= size) {
+	node<T> *getnode(int index) const
+	{
+		if (index < 0 || index >= size)
+		{
 			throw std::out_of_range("Index out of range");
 		}
-		node<T>* current = head.get();
-		for (int i = 0; i < index; ++i) {
+		node<T> *current = head.get();
+		for (int i = 0; i < index; ++i)
+		{
 			current = current->getnextnode();
 		}
 		return current;
 	}
 
-	T operator[](int index) const {
+	T operator[](int index) const
+	{
 		return this->getnode(index)->getvalue();
 	}
 };
